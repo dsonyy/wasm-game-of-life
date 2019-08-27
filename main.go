067893ca18181de78767decad96f478bdf0192c0
Szymon Bednorz <dsonyyy@gmail.com>
2019-08-27 13:13:00 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"syscall/js"
+	"time"
 )
 
-const backgroundColor = "#111111"
-const color = "#000000"
+const backgroundColor = "#555"
+const color = "#eee"
 
 type Game struct {
 	board  [][]bool
@@ -86,17 +88,16 @@ func render(g Game) {
 	// }
 }
 
-var games map[string]Game
+var games map[int]Game
 
 func initCanvases() {
 	canvases := js.Global().Get("document").Call("querySelectorAll", "[data-conways]")
-	games = make(map[string]Game)
+	games = make(map[int]Game)
 
 	for i := 0; i < canvases.Length(); i++ {
-		cell := 10
+		cell := 3
 		width := canvases.Index(i).Get("width").Int() / cell
 		height := canvases.Index(i).Get("height").Int() / cell
-		id := canvases.Index(i).Get("id").String()
 
 		game := Game{[][]bool{}, width, height, cell}
 		for y := 0; y < height; y++ {
@@ -106,10 +107,7 @@ func initCanvases() {
 			}
 		}
 
-		game.board[2][2] = true
-		game.board[6][5] = true
-
-		games[id] = game
+		games[i] = game
 	}
 }
 
@@ -117,44 +115,44 @@ func fillCanvases(percentage int) {
 	canvases := js.Global().Get("document").Call("querySelectorAll", "[data-conways]")
 
 	for i := 0; i < canvases.Length(); i++ {
-		id := canvases.Index(i).Get("id").String()
 
-		for y := 0; y < games[id].height; y++ {
-			for x := 0; x < games[id].width; x++ {
-				games[id].board[y][x] = percentage > rand.Intn(100)
+		for y := 0; y < games[i].height; y++ {
+			for x := 0; x < games[i].width; x++ {
+				games[i].board[y][x] = percentage > rand.Intn(100)
 			}
 		}
 	}
 
 }
 
-func countNeighbours(g *Game, x int, y int) int {
+func countNeighbours(i int, x int, y int) int {
 	count := 0
 
-	if x > 0 && g.board[x-1][y] {
+	if x > 0 && games[i].board[x-1][y] {
 		count = count + 1
 	}
-	if x < edge-1 && g.board[x+1][y] {
-		count = count + 1
-	}
-
-	if y > 0 && g.board[x][y-1] {
-		count = count + 1
-	}
-	if y < edge-1 && g.board[x][y+1] {
+	if x < games[i].width-1 && games[i].board[x+1][y] {
 		count = count + 1
 	}
 
-	if x > 0 && y > 0 && g.board[x-1][y-1] {
+	if y > 0 && games[i].board[x][y-1] {
 		count = count + 1
 	}
-	if x < edge-1 && y > 0 && g.board[x+1][y-1] {
+
+	if y < games[i].height-1 && games[i].board[x][y+1] {
 		count = count + 1
 	}
-	if x > 0 && y < edge-1 && g.board[x-1][y+1] {
+
+	if x > 0 && y > 0 && games[i].board[x-1][y-1] {
 		count = count + 1
 	}
-	if x < edge-1 && y < edge-1 && g.board[x+1][y+1] {
+	if x < games[i].width-1 && y > 0 && games[i].board[x+1][y-1] {
+		count = count + 1
+	}
+	if x > 0 && y < games[i].height-1 && games[i].board[x-1][y+1] {
+		count = count + 1
+	}
+	if x < games[i].width-1 && y < games[i].height-1 && games[i].board[x+1][y+1] {
 		count = count + 1
 	}
 
@@ -163,13 +161,33 @@ func countNeighbours(g *Game, x int, y int) int {
 
 func updateCanvases() {
 
-	for id, game := range games {
-		for y := 0; y < game.height; y++ {
-			for x := 0; x < game.width; x++ {
-				if (countNeighbours(&game, x, y) == 3 )
+	for i := range games {
+
+		updated := Game{[][]bool{}, games[i].width, games[i].height, games[i].cell}
+		for y := 0; y < updated.height; y++ {
+			updated.board = append(updated.board, []bool{})
+			for x := 0; x < updated.width; x++ {
+				updated.board[y] = append(updated.board[y], false)
 			}
 		}
+
+		start := time.Now()
+
+		for y := 0; y < games[i].height; y++ {
+			for x := 0; x < games[i].width; x++ {
+				if countNeighbours(i, x, y) == 3 {
+					updated.board[x][y] = true
+				} else if games[i].board[x][y] && countNeighbours(i, x, y) == 2 {
+					updated.board[x][y] = true
+				}
+			}
+		}
+		games[i] = updated
+
+		elapsed := time.Since(start)
+		log.Printf("%s", elapsed)
 	}
+
 }
 
 func renderCanvases() {
@@ -177,15 +195,16 @@ func renderCanvases() {
 
 	for i := 0; i < canvases.Length(); i++ {
 		context := canvases.Index(i).Call("getContext", "2d")
-		id := canvases.Index(i).Get("id").String()
 
-		for y := 0; y < games[id].height; y++ {
-			for x := 0; x < games[id].width; x++ {
-				if games[id].board[x][y] == true {
+		context.Set("fillStyle", backgroundColor)
+		context.Call("fillRect", 0, 0, games[i].width*games[i].cell, games[i].height*games[i].cell)
 
-					context.Call("rect", x*10, y*10, 10, 10)
+		for y := 0; y < games[i].height; y++ {
+			for x := 0; x < games[i].width; x++ {
+				if games[i].board[x][y] == true {
+
 					context.Set("fillStyle", color)
-					context.Call("fill")
+					context.Call("fillRect", x*games[i].cell, y*games[i].cell, games[i].cell, games[i].cell)
 				}
 			}
 		}
@@ -197,10 +216,48 @@ func main() {
 	fmt.Println("Hello, WebAssembly!")
 
 	initCanvases()
+	fillCanvases(30)
+
+	for {
+		// str := ""
+		// for y := 0; y < games[0].height; y++ {
+		// 	for x := 0; x < games[0].width; x++ {
+		// 		if games[0].board[x][y] {
+		// 			str += "[" + strconv.FormatInt(int64(countNeighbours(0, x, y)), 10) + "]"
+		// 		} else {
+		// 			str += " " + strconv.FormatInt(int64(countNeighbours(0, x, y)), 10) + " "
+		// 		}
+		// 	}
+		// 	str += "\n"
+		// }
+		// fmt.Println(str)
+
+		updateCanvases()
+		renderCanvases()
+
+		time.Sleep(500 * time.Millisecond)
+	}
+
 	//fmt.Println(games)
-	fillCanvases(10)
-	//fmt.Println(games)
-	renderCanvases()
+	//fillCanvases(10)
+
+	//for i := int64(0); true; i++ {
+	//renderCanvases()
+	// rand.Seed(1)
+	// fillCanvases(10)
+
+	// renderCanvases()
+	// rand.Seed(1)
+	// fillCanvases(10)
+
+	// renderCanvases()
+	// rand.Seed(1)
+	// fillCanvases(10)
+
+	// renderCanvases()
+	// rand.Seed(111)
+	// fillCanvases(0)
+	//}
 
 	<-c
 }
